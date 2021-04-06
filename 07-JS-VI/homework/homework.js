706a28fe20ae01bf2a9e8ab6cab10bea81391e50
Argenthands <alejandro.david.benolol@gmail.com>
2021-04-06 01:14:15 -0300

    correjido el ejercicio de map de la guia 7 para que este hecho con map, el return del ejercicio de filter tambien fue corregido en la misma linea que se declara la arrow function

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -43,6 +43,7 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  return result = array.map(element => cb(element));
   array2 = [];
   array.forEach((element,index) => array2[index] = cb(element));
   return array2;  
@@ -52,8 +53,7 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-  filterArray = array.filter(element => element.charAt(0) === 'a');
-  return filterArray;
+  return filterArray = array.filter(element => element.charAt(0) === 'a');
 }
 
 // No modificar nada debajo de esta línea
